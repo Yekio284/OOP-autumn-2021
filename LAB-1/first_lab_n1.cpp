0adef98c1fb292e15e394230edfe797dbce0772d
Yekio284 <yan.uryanskij@csc.sibsutis.ru>
2021-09-22 22:54:03 +0700

    First lab work

---
+++ LAB-1/first_lab_n1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int *genRandArray(int size, int maxValue) {
+    srand(time(NULL));
+    int *arr = new int[size+1];
+    arr[0] = size;
+    for(int i = 1; i <= size; i++) { 
+        arr[i] = rand() % (maxValue); 
+    }
+    return arr;
+}
+
+void print(int *arr) {
+    cout << arr[0] << ": ";
+    for(int i = 1; i <= arr[0]; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    srand(time(NULL));
+    int size = rand() % 10;
+    int maxValue = 100;
+    int *arr = genRandArray(size, maxValue);
+    print(arr);
+    
+    delete[] arr;
+    return 0;
+}
---
+++ LAB-1/first_lab_n2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+int **genRandMatrix(int size, int maxValue) {
+    srand(time(NULL));
+    int **matrix = new int* [size + 1];
+    int i, j;
+    matrix[0] = new int[1];
+    matrix[0][0] = size;
+    
+    for(i = 1; i <= matrix[0][0]; i++) {
+        j = 0;
+        int k = rand() % 10;
+        matrix[i] = new int[k + 1];
+        matrix[i][j] = k;
+        
+        for(j = 1; j <= matrix[i][0]; j++) {
+            matrix[i][j] = rand() % maxValue;
+        }
+    }
+    return matrix;
+}
+
+void printMatrix(int **matrix) {
+    cout << matrix[0][0] << endl;
+    int i, j;
+    for(i = 1; i <= matrix[0][0]; i++) {
+        cout << matrix[i][0] << ": ";
+        for(j = 1; j <= matrix[i][0]; j++) {
+            cout << setw(4) << matrix[i][j]; 
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    srand(time(NULL));
+    int size = rand() % 10;
+    int maxValue = 100;
+    int **matrix = genRandMatrix(size, maxValue);
+    printMatrix(matrix);
+
+    for(int i = 0; i < size; i++)
+        delete[] matrix[i];
+    
+    delete[] matrix;
+    return 0;
+}
