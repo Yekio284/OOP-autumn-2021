898d9a8bb1b7b02296ba0875587dc281601bc92f
Yekio284 <yan.uryanskij@csc.sibsutis.ru>
2021-09-26 19:49:14 +0700

    Finished up_left_spiral function

--- LAB-2/second_lab_n1.cpp
+++ LAB-2/second_lab_n1.cpp
@@ -14,7 +14,30 @@ int **genRandMatrix(int N, int maxValue) {
         for(j = 0; j < N; j++)
             matrix[i][j] = rand() % maxValue;
     }
+    
     return matrix;
+}
+
+int *up_left_spiral(int **matrix, int *D, int N) {
+    int i, j, k;
+    int limit = N / 2;
+    int count = 0;
+
+    for (k = 0; k < limit; k++) {
+        for (i = k; i < N - k; i++, count++)
+            D[count] = matrix[i][k];
+        for (j = k + 1; j < N - k; j++, count++)
+            D[count] = matrix[i - 1][j];
+        for (i = N - k - 2; i >= k; i--, count++) 
+            D[count] = matrix[i][j - 1];
+        for (j = N - k - 2; j >= k + 1; j--, count++)
+            D[count] = matrix[i + 1][j];
+    }
+
+    if (N % 2 != 0)
+        D[count] = matrix[limit][limit];
+
+    return D;
 }
 
 void printMatrix(int **matrix, int N) {
@@ -28,15 +51,11 @@ void printMatrix(int **matrix, int N) {
     }
 }
 
-void printArray(int *D, int N) {
-    cout << "Our Array:" << endl;
-    
+void printArray(int *D, int N) {    
     for(int i = 0; i < N; i++)
         cout << setw(4) << D[i];
     cout << endl;
 }
-
-int *up_left_spiral(int **matrix, int *D, int N); //d
 
 int main() {
     srand(time(NULL));
@@ -46,6 +65,9 @@ int main() {
     int *D = new int[N * N];
     
     printMatrix(matrix, N);
+    D = up_left_spiral(matrix, D, N);
+    cout << "Up-left spiral:" << endl;
+    printArray(D, N * N);
 
     for(int i = 0; i < N; i++)
         delete[] matrix[i];
