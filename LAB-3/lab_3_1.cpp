3a7f4680be33c762ee8a3117be74a14d844f2525
Yekio284 <yan.uryanskij@csc.sibsutis.ru>
2021-10-16 00:08:31 +0700

    Took out the definition of class tPoint from function main to .hpp file

--- LAB-3/lab_3_1.cpp
+++ LAB-3/lab_3_1.cpp
@@ -1,84 +1,9 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <iostream>
+#include "tPoint.hpp"
 
 using namespace sf;
 using namespace std;
-
-class tPoint {
-private:
-	int x_speed = 1, y_speed = 1;
-	int x_coordinate, y_coordinate, color_red, color_green, color_blue;
-
-public:
-	void set_x_coordinate(int x) {
-		x_coordinate = x;
-	}
-	void set_y_coordinate(int y) {
-		y_coordinate = y;
-	}
-	int get_x_coordinate() {
-		return x_coordinate;
-	}
-	int get_y_coordinate() {
-		return y_coordinate;
-	}
-	void set_color_red(int red) {
-		color_red = red;
-	}
-	void set_color_green(int grn) {
-		color_green = grn;
-	}
-	void set_color_blue(int blue) {
-		color_blue = blue;
-	}
-	int get_color_red() {
-		return color_red;
-	}
-	int get_color_green() {
-		return color_green;
-	}
-	int get_color_blue() {
-		return color_blue;
-	}
-	void linearmove_x() {
-		x_coordinate = x_coordinate + x_speed;
-		if (x_coordinate < 0 || x_coordinate > 1910)
-			x_speed = x_speed * -1;
-	}
-	void linearmove_y() {
-		y_coordinate = y_coordinate + y_speed;
-		if (y_coordinate < 0 || y_coordinate > 1070)
-			y_speed = y_speed * -1;
-	}
-	void randommove_x() {
-		int variant_to_go = 1 + rand() % 2; // [1;2]
-
-		if (x_coordinate < 0)
-			x_coordinate = x_coordinate + x_speed * 2;
-		else if (x_coordinate > 1910)
-			x_coordinate = x_coordinate - x_speed * 2;
-		else {
-			if (variant_to_go == 1)
-				x_coordinate = x_coordinate + x_speed * 2;
-			else
-				x_coordinate = x_coordinate + x_speed * -2;
-		}
-	}
-	void randommove_y() {
-		int variant_to_go = 1 + rand() % 2; // [1;2]
-
-		if (y_coordinate < 0)
-			y_coordinate = y_coordinate + y_speed * 2;
-		else if (y_coordinate > 1070)
-			y_coordinate = y_coordinate - y_speed * 2;
-		else {
-			if (variant_to_go == 1)
-				y_coordinate = y_coordinate + y_speed * 2;
-			else
-				y_coordinate = y_coordinate + y_speed * -2;
-		}
-	}
-};
 
 int main() {
 	srand(time(NULL));
