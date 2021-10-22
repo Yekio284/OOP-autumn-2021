f08a17e128f83a1aa2679138ed02d2f38689388d
Yekio284 <yan.uryanskij@csc.sibsutis.ru>
2021-10-22 22:22:24 +0700

    Almost done

--- LAB-4/lab4.cpp
+++ LAB-4/lab4.cpp
@@ -92,9 +92,72 @@ public:
 	}
 };
 
-class Line : public tPoint {};
+class EllipseShape : public Shape {
+private:
+	Vector2f m_radius;
 
-class Circle : public tPoint {};
+public:
+	explicit EllipseShape(const Vector2f& radius = Vector2f(0, 0)) : m_radius(radius) {
+		update();
+	}
+	void setRadius(const Vector2f& radius) {
+		m_radius = radius;
+		update();
+	}
+	const Vector2f& getRadius() const {
+		return m_radius;
+	}
+	virtual unsigned int getPointCount() const {
+		return 30; // fixed, but could be an attribute of the class if needed
+	}
+	virtual Vector2f getPoint(unsigned int index) const {
+		static const float pi = 3.141592654f;
+
+		float angle = index * 2 * pi / getPointCount() - pi / 2;
+		float x = std::cos(angle) * m_radius.x;
+		float y = std::sin(angle) * m_radius.y;
+
+		return Vector2f(m_radius.x + x, m_radius.y + y);
+	}
+};
+
+class Line : public tPoint {
+private:
+	int thickness_1, thickness_2, rotation_degree;
+
+public:
+	Line(int Thickness1, int Thickness2, int RotationDegree) : thickness_1(Thickness1), thickness_2(Thickness2), rotation_degree(RotationDegree) {
+		cout << "Constructor Line\n";
+	}
+	int get_thickness_1() {
+		return thickness_1; // roughly speaking, this is length
+	}
+	int get_thickness_2() {
+		return thickness_2; // and this is height
+	}
+	int get_rotation_degree() {
+		return rotation_degree;
+	}
+	~Line() {
+		cout << "Destructor Line\n";
+	}
+};
+
+class Circle : public tPoint {
+private:
+	int radius;
+
+public:
+	Circle(int Radius) : radius(Radius) {
+		cout << "Constructor Circle\n";
+	}
+	int get_radius() {
+		return radius;
+	}
+	~Circle() {
+		cout << "Destructor Circle\n";
+	}
+};
 
 class Rectangle : public tPoint {
 private: 
@@ -115,82 +178,196 @@ public:
 	}
 };
 
-class Triangle : public tPoint {};
+class Triangle : public tPoint {
+private:
+	int space, num_of_angles;
 
-class Elipse : public tPoint {};
+public:
+	Triangle(int Space, int NumOfAngles) : space(Space), num_of_angles(NumOfAngles) {
+		cout << "Constructor Triangle\n";
+	}
+	int get_space() {
+		return space;
+	}
+	int get_num_of_angles() {
+		return num_of_angles;
+	}
+	~Triangle() {
+		cout << "Destructor Triangle\n";
+	}
+};
 
-class Rhombus : public tPoint {};
+class Ellipse : public tPoint {
+private:
+	int radius, convexity;
+
+public:
+	Ellipse(int Radius, int Convexity) : radius(Radius), convexity(Convexity) {
+		cout << "Constructor Ellipse\n";
+	}
+	int get_radius() {
+		return radius;
+	}
+	int get_convexity() {
+		return convexity;
+	}
+	~Ellipse() {
+		cout << "Destructor Ellipse\n";
+	}
+};
+
+class Rhombus : public tPoint {
+private:
+	int length, height;
+
+public:
+	Rhombus(int Length, int Height) : length(Length), height(Height) {
+		cout << "Constructor Rhombus\n";
+	}
+	int get_length() {
+		return length;
+	}
+	int get_height() {
+		return height;
+	}
+	~Rhombus() {
+		cout << "Destructor Rhombus\n";
+	}
+};
 
 int main() {
-	srand(time(NULL));
-	tPoint point(1, 1, (rand() % 1910), (rand() % 1070), (rand() % 255), (rand() % 255), (rand() % 255));
-	Rectangle myRectangle((30 + rand() % 71), (30 + rand() % 71)); // [30; 100]
-
-	/*int user_choose, i;
-	cout << "What do you want to see: linear move or random move? (1/2): ";
+	int user_choose;
+	cout << "What do you want to see: linear move or rotation? (1/2): ";
 	cin >> user_choose;
-	*/
+	if (user_choose != 1 && user_choose != 2) {
+		cout << "Invalid number\n";
+		return 1;
+	}
+	
+	srand(time(NULL));
+	
+	cout << "Constructors:\n";
+
+	tPoint tpoint(1, 1, (rand() % 1910), (rand() % 1070), (rand() % 255), (rand() % 255), (rand() % 255)); 
+	Line myLine((60 + rand() % 41), (3 + rand() % 3), (rand() % 21)); // [60; 100], [3; 5], [0; 20]
+	Circle myCircle(30 + rand() % 31); // [30; 60]
+	Rectangle myRectangle((30 + rand() % 71), (30 + rand() % 71)); // [30; 100]
+	Triangle myTriangle((15 + rand() % 36), 3); // [15; 50]
+	Ellipse myEllipse((30 + rand() % 31), (15 + rand() % 36)); // [30; 60], [15; 50]
+	int side_of_rhombus = 30 + rand() % 31; // [30; 100]
+	Rhombus myRhombus(side_of_rhombus, side_of_rhombus); 
+	
 
 	RenderWindow window(VideoMode(1920, 1080), "LAB-4");
 
-	// Главный цикл приложения. Выполняется, пока открыто окно
+
+	float rotation = 0;
+	// Main loop of app. Performed while window is open.
 	while (window.isOpen()) {
-		// Обрабатываем очередь событий в цикле
+		// Processing the event queue in a loop
 		Event event;
 		while (window.pollEvent(event)) {
-			// Пользователь нажал на «крестик» и хочет закрыть окно?
-			if (event.type == Event::Closed)
-				// тогда закрываем его
+			// User wants to close the window?
+			if (event.type == Event::Closed) {
+				cout << "\nDestructors:\n";
+				// then close it
 				window.close();
+			}
 		}
-		// Цвет окна
+		// The color of window
 		window.clear(Color(0, 0, 0, 0));
 
-		CircleShape circle(10.f);
-		circle.setPosition(point.get_x(), point.get_y());
-		circle.setFillColor(Color(point.get_color_red(), point.get_color_green(), point.get_color_blue()));
-		window.draw(circle);
-
-		/*RectangleShape rectangle(Vector2f((myRectangle.get_length()), (myRectangle.get_height())));
-		rectangle.setPosition(myRectangle.get_x(), myRectangle.get_y());
-		rectangle.setFillColor(Color(myRectangle.get_color_red(), myRectangle.get_color_green(), myRectangle.get_color_blue()));
-		*/
-		RectangleShape rectangle(Vector2f((myRectangle.get_length()), (myRectangle.get_height())));
+		CircleShape point(10.f);
+		point.setPosition(tpoint.get_x(), tpoint.get_y());
+		point.setFillColor(Color(tpoint.get_color_red(), tpoint.get_color_green(), tpoint.get_color_blue()));
+		
+		RectangleShape line(Vector2f(myLine.get_thickness_1(), myLine.get_thickness_2()));
+		line.setPosition(myLine.get_x(), myLine.get_y());
+		line.setFillColor(Color(myLine.get_color_red(), myLine.get_color_green(), myLine.get_color_blue()));
+		line.rotate(myLine.get_rotation_degree());
+		
+		CircleShape circle(myCircle.get_radius());
+		circle.setPosition(myCircle.get_x(), myCircle.get_y());
+		circle.setFillColor(Color(myCircle.get_color_red(), myCircle.get_color_green(), myCircle.get_color_blue()));
+		
+		RectangleShape rectangle(Vector2f(myRectangle.get_length(), myRectangle.get_height()));
 		rectangle.setPosition(myRectangle.get_x(), myRectangle.get_y());
 		rectangle.setFillColor(Color(myRectangle.get_color_red(), myRectangle.get_color_green(), myRectangle.get_color_blue()));
 		
-		myRectangle.linearmove_x();
-		myRectangle.linearmove_y();
-		window.draw(rectangle);
+		CircleShape triangle(myTriangle.get_space(), myTriangle.get_num_of_angles());
+		triangle.setPosition(myTriangle.get_x(), myTriangle.get_y());
+		triangle.setFillColor(Color(myTriangle.get_color_red(), myTriangle.get_color_green(), myTriangle.get_color_blue()));
+		
+		EllipseShape ellipse(Vector2f(myEllipse.get_radius(), myEllipse.get_convexity()));
+		ellipse.setPosition(myEllipse.get_x(), myEllipse.get_y());
+		ellipse.setFillColor(Color(myEllipse.get_color_red(), myEllipse.get_color_green(), myEllipse.get_color_blue()));
+		
+		RectangleShape rhombus(Vector2f(myRhombus.get_length(), myRhombus.get_height()));
+		rhombus.setPosition(myRhombus.get_x(), myRhombus.get_y());
+		rhombus.setFillColor(Color(myRhombus.get_color_red(), myRhombus.get_color_green(), myRhombus.get_color_blue()));
+		rhombus.rotate(45);
+		
 
-		//window.draw(circle);
-		//window.draw(rectangle);
-		/*
-		for (i = 0; i < 100; i++) {
-			CircleShape circle(10.f);
-			circle.setPosition(arr_of_points[i].get_x(), arr_of_points[i].get_y());
-			circle.setFillColor(Color(arr_of_points[i].get_color_red(), arr_of_points[i].get_color_green(), arr_of_points[i].get_color_blue()));
+		switch (user_choose) {
+		case 1: 
+			tpoint.linearmove_x();
+			tpoint.linearmove_y();
+			window.draw(point);
 
-			switch (user_choose) {
-			case 1:
-				arr_of_points[i].linearmove_x();
-				arr_of_points[i].linearmove_y();
-				break;
-			case 2:
-				arr_of_points[i].randommove_x();
-				arr_of_points[i].randommove_y();
-				break;
-			default:
-				cout << "Incorrect input.";
-				return 1;
-			}
+			myLine.linearmove_x();
+			myLine.linearmove_y();
+			window.draw(line);
 
+			myCircle.linearmove_x();
+			myCircle.linearmove_y();
 			window.draw(circle);
-		}
-		*/
 
-		// Отрисовка окна
+			myRectangle.linearmove_x();
+			myRectangle.linearmove_y();
+			window.draw(rectangle);
+
+			myTriangle.linearmove_x();
+			myTriangle.linearmove_y();
+			window.draw(triangle);
+
+			myEllipse.linearmove_x();
+			myEllipse.linearmove_y();
+			window.draw(ellipse);
+
+			myRhombus.linearmove_x();
+			myRhombus.linearmove_y();
+			window.draw(rhombus);
+			
+			break;
+
+		case 2:
+			point.rotate(rotation);
+			window.draw(point);
+
+			line.rotate(rotation);
+			window.draw(line);
+
+			circle.rotate(rotation);
+			window.draw(circle);
+
+			rectangle.rotate(rotation);
+			window.draw(rectangle);
+
+			triangle.rotate(rotation);
+			window.draw(triangle);
+
+			ellipse.rotate(rotation);
+			window.draw(ellipse);
+
+			rhombus.rotate(rotation);
+			window.draw(rhombus);
+
+			break;
+		}
+
+		// Display the window
 		window.display();
+		rotation = rotation + 0.01;
 	}
 
 	return 0;
